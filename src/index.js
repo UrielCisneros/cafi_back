ba870d37d9a68596b70d34d92cbb1bf0bb648de7
UrielCisneros <urielcisne96@gmail.com>
2020-10-17 13:54:59 -0500

    subiendo server]

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ require('./database');
 
 
 async function main() {
-    await app.listen(process.env.PORT || '0.0.0.0', process.env.HOST || '0.0.0.0', () => {
-        console.log('Server on port ', process.env.PORT || '0.0.0.0');
-    }); 
+    try {
+        await app.listen(process.env.PORT || '0.0.0.0', process.env.HOST || '0.0.0.0', () => {
+            console.log('Server on port ', process.env.PORT || '0.0.0.0');
+        }); 
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 
